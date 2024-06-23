fb87d7d47dfcb8cfbd81b2c05df920f6d1e6c721
Artem Krasov <artkrasov@gmail.com>
2024-06-23 17:48:33 +0300

    фиксирую

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink to="/" class="mb-10 black">
+      <NuxtImg src="/logo.svg" alt="" width="100px" />
+    </NuxtLink>
+    <UiButton>Send</UiButton>
+  </aside>
+</template>
+
+<style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: false },
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/google-fonts",
+  ],
+  shadcn: {
+    prefix: "Ui",
+    componentDir: "./components/ui",
+  },
+});
